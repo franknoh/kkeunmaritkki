7727730997d6f766e85ba38fea5e4807f6ae9040
franknoh <42113651+franknoh@users.noreply.github.com>
2020-09-14 11:25:37 +0900

    Create firebase-init.js

---
+++ src/firebase-init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBFOHUkMlrcc4-R1_vuW4qpIx5K729GkrE",
+    authDomain: "kkeunmaritkki.firebaseapp.com",
+    databaseURL: "https://kkeunmaritkki.firebaseio.com",
+    projectId: "kkeunmaritkki",
+    storageBucket: "kkeunmaritkki.appspot.com",
+    messagingSenderId: "869460943018",
+    appId: "1:869460943018:web:ecb81830cff4cca120b0d3",
+    measurementId: "G-FX5NPHWB8B"
+};
