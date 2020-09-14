4c9e18206f42646ec1a0be9533f52d4b08766e37
franknoh <42113651+franknoh@users.noreply.github.com>
2020-09-14 13:48:11 +0900

    Update naver-init.js

--- src/naver-init.js
+++ src/naver-init.js
@@ -1,9 +1,9 @@
 var naverLogin = new naver.LoginWithNaverId(
-	{
-  	clientId: "0WJ_zopvYiVZQOBDhoRw",
+  {
+    clientId: "0WJ_zopvYiVZQOBDhoRw",
     callbackUrl: "https://kkeunmaritkki.firebaseapp.com/__/auth/handler",
     isPopup: true,
-    loginButton: {color: "green", type: 3, height: 60}
+    callbackHandle: false
   }
 );
 naverLogin.init();
