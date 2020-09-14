af3ccef98815b7613e30be9ec6f15bedc49539c7
franknoh <42113651+franknoh@users.noreply.github.com>
2020-09-14 12:57:48 +0900

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,11 @@
-
+function getUser() {
+  if(!!firebase.auth().currentUser){// firebase를 사용하는지 감지
+     
+  }else if(!!naverLogin.user.getId()){// 네이버 로그인 감지
+     
+  }else if(!!Kakao.Auth.getAccessToken()){// 카카오 로그인 감지
+     
+  }else{
+     return false;
+  }
+}
