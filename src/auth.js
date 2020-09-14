44a9a03199d4bdcfee79c3928b044a267161ee13
franknoh <42113651+franknoh@users.noreply.github.com>
2020-09-14 13:11:11 +0900

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,25 @@
 function getUser() {
   if(!!firebase.auth().currentUser){// firebase를 사용하는지 감지
-     
+     return {
+       'provider': firebase.auth().currentUser.providerData[0].providerId,
+       'profile': firebase.auth().currentUser.providerData[0].photoURL,
+       'name': firebase.auth().currentUser.providerData[0].displayName,
+       'uid': firebase.auth().currentUser.uid
+     };
   }else if(!!naverLogin.user.getId()){// 네이버 로그인 감지
-     
+     return {
+       'provider': 'naver',
+       'profile': naverLogin.user.getProfileImage(),
+       'name': naverLogin.user.getNickName(),
+       'uid': naverLogin.user.getId()
+     };
   }else if(!!Kakao.Auth.getAccessToken()){// 카카오 로그인 감지
-     
+     /*return {
+       'provider': 'kakao',
+       'profile': firebase.auth().currentUser.providerData[0].photoURL,
+       'name': firebase.auth().currentUser.providerData[0].displayName,
+       'uid': firebase.auth().currentUser.uid
+     };*/
   }else{
      return false;
   }
